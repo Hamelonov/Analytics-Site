5db4d797eb996e2d6a3b82387e5be9e83ca1dd49
Hamelonov <124854475+Hamelonov@users.noreply.github.com>
2025-12-29 18:26:59 +0500

    Refactor main.js to improve structure and logging

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
+alert("Логическая ширина: " + window.screen.width + "px\n" + "Device Pixel Ratio (DPR): " + window.devicePixelRatio);
+console.log('Test 123');
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
-	alert(
-  "Логическая ширина: " + window.screen.width + "px\n" +
-  "Device Pixel Ratio (DPR): " + window.devicePixelRatio
-);
 });
+
